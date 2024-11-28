d17ffa8fedf4d5dc2665f00c3d48fa5e3ddf900c
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2024-11-27 21:49:27 -0300

    fix: unusued params removed + toString

--- src/Account/CheckingAcc.java
+++ src/Account/CheckingAcc.java
@@ -1,13 +1,10 @@
 package Account;
-import java.time.LocalDate;
-import java.util.LinkedList;
-
 import User.User;
 
 public class CheckingAcc extends Account {
 
-    public CheckingAcc(User user, String accType, int accNumber, double balance, LocalDate openingDate) {
-        super(user, "Checking Account", accNumber, balance, openingDate);
+    public CheckingAcc(User user, String accType, int accNumber, double balance) {
+        super(user, "Checking Account", accNumber, balance);
     }
 
 }
